b7852b31940b58b01f053051fe977d1106b86801
Leonardo Hernandez <Leo.tigre19@gmail.com>
2026-05-02 14:34:43 -0500

    added input to move car left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    private float _horizontalInput;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _horizontalInput = Input.GetAxis("Horizontal");
         //We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * _horizontalInput);
     }
 }
